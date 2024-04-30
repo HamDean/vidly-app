f58294e80d59363dbdd470befce49ae5506ce4a4
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-04-30 21:35:17 +0000

    handle POST request

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const Joi = require("joi");
+const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 const genres = [
-    {id: 1, genre: 'adventure'},
-    {id: 2, genre: 'fantasy'},
-    {id: 3, genre: 'horro'},
-    {id: 4, genre: 'comedy'},
-]
+  { id: 1, genre: "adventure" },
+  { id: 2, genre: "fantasy" },
+  { id: 3, genre: "horro" },
+  { id: 4, genre: "comedy" },
+];
 
 const lookUpGenre = (id) => {
-    return genres.find(genre => genre.id === parseInt(id))
-}
+  return genres.find((genre) => genre.id === parseInt(id));
+};
 
-app.get('/api/genres', (req, res) => {
-    res.send(genres)
-})
+const schema = Joi.object({
+  genre: Joi.string().required(),
+});
 
-app.get('/api/genres/:id', (req, res) => {
-    const genre = lookUpGenre(req.params.id)
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
 
-    if(!genre) return res.status(404).send('No genre found :(')
+app.get("/api/genres/:id", (req, res) => {
+  const genre = lookUpGenre(req.params.id);
 
-    res.send(genre)
-})
+  if (!genre) return res.status(404).send("No genre found :(");
 
-app.listen(port, () => console.log(`listening on port ${port}`))
+  res.send(genre);
+});
+
+app.post("/api/genres", (req, res) => {
+  const newGenre = req.body;
+
+  const { error } = schema.validate(newGenre);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    genre: newGenre.genre,
+  };
+
+  genres.push(genre);
+
+  res.send(genre);
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
