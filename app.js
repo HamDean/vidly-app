cafd83a48c0204f733d60498bceaaf8304aaea2f
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-04-30 21:14:28 +0000

    handle get genre by ID route

--- app.js
+++ app.js
@@ -9,8 +9,20 @@ const genres = [
     {id: 4, genre: 'comedy'},
 ]
 
+const lookUpGenre = (id) => {
+    return genres.find(genre => genre.id === parseInt(id))
+}
+
 app.get('/api/genres', (req, res) => {
     res.send(genres)
+})
+
+app.get('/api/genres/:id', (req, res) => {
+    const genre = lookUpGenre(req.params.id)
+
+    if(!genre) return res.status(404).send('No genre found :(')
+
+    res.send(genre)
 })
 
 app.listen(port, () => console.log(`listening on port ${port}`))
