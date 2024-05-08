d367e21aee79b73c29a51b760ac09f13ef401ecb
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-08 16:18:49 +0000

    setting up the movies endpoint

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const genres = require("./routes/genres");
 const customers = require('./routes/cutomers')
+const movies = require('./routes/movies')
 
 mongoose
   .connect("mongodb://localhost:27017/vidly")
@@ -13,6 +14,7 @@ mongoose
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers)
+app.use("/api/movies", movies)
 
 const port = process.env.PORT || 3000;
 
