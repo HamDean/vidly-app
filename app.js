aa326304f407e27b7ef479a3b91a737778c98c7f
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-09 08:47:24 +0000

    set up rental route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const genres = require("./routes/genres");
 const customers = require('./routes/cutomers')
 const movies = require('./routes/movies')
+const rentals = require('./routes/rentals')
 
 mongoose
   .connect("mongodb://localhost:27017/vidly")
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers)
 app.use("/api/movies", movies)
+app.use("/api/rentals", rentals)
 
 const port = process.env.PORT || 3000;
 
