3789350017cd53e3d9659f8802b56b2eca92364a
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-09 10:32:09 +0000

    final touches to rental route handler

--- routes/rentals.js
+++ routes/rentals.js
@@ -3,7 +3,7 @@ const Movie = require("../models/movie");
 const Rental = require("../models/rentals");
 const express = require("express");
 const { rentalSchema } = require("../schema");
-const { badRequest } = require("../utils");
+const { badRequest, notFound } = require("../utils");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -16,21 +16,32 @@ router.post("/", async (req, res) => {
   if (error) return badRequest(res, error);
 
   const customer = await Customer.findById(req.body.customerId);
+  if (!customer) return notFound(res, "customer");
+
   const movie = await Movie.findById(req.body.movieId);
+  if (!movie) return notFound(res, "movie");
+
+  if (movie.numberInStock === 0)
+    return res.status(400).send("Movie not in stock");
 
   const rental = new Rental({
     customer: {
+      _id: customer.id,
       name: customer.name,
       isGold: customer.isGold,
       phone: customer.phone,
     },
     movie: {
+      _id: movie.id,
       title: movie.title,
       dailyRentalRate: movie.dailyRentalRate,
     },
   });
 
   await rental.save();
+
+  movie.numberInStock--;
+  await movie.save();
 
   res.send(rental);
 });
