403d660b6efcc86cb4cdda8f3a25e1998131c9c2
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-09 21:55:34 +0000

    installing and using lodash

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 const User = require("../models/user");
 const express = require("express");
 const { userSchema } = require("../schema");
@@ -9,15 +10,11 @@ router.post("/", async (req, res) => {
   if (error) return badRequest(res, error);
 
   try {
-    const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    const user = new User(_.pick(req.body, ["name", "email", "password"]));
 
     await user.save();
 
-    res.send(user);
+    res.send(_.pick(user, ["_id", "name", "email"]));
   } catch (error) {
     return res.status(400).send("User already exists");
   }
