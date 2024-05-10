2b72d29ae472726fcda659aa89aab07044e8b873
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-10 11:18:16 +0000

    adding response headers

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const config = require('config')
+const jwt = require('jsonwebtoken')
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const User = require("../models/user");
@@ -10,18 +12,20 @@ router.post("/", async (req, res) => {
   const { error } = userSchema.validate(req.body);
   if (error) return badRequest(res, error);
 
-  try {
-    const user = new User(_.pick(req.body, ["name", "email", "password"]));
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already exist");
 
-    const salt = await bcrypt.genSalt(10);
-    const hashed = await bcrypt.hash(user.password, salt)
-    user.password = hashed;
-    await user.save();
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
 
-    res.send(_.pick(user, ["_id", "name", "email"]));
-  } catch (error) {
-    return res.status(400).send("User already exists");
-  }
+  const salt = await bcrypt.genSalt(10);
+  const hashed = await bcrypt.hash(user.password, salt);
+  user.password = hashed;
+  await user.save();
+
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
